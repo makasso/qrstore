b3ff675be94003b74438074bfadc9c0823f0dc57
makasso15 <makasso.dev@gmail.com>
2021-10-07 12:27:14 +0100

    Initial commit

---
+++ vendor/check/src/Everify.php
@@ -0,0 +1,21 @@
+<?php 
+namespace Amcoders\Check;
+use Illuminate\Support\Facades\Http;
+use Amcoders\Lpress\Lphelper;
+
+class Everify 
+{
+	public static $massage;
+	
+	public static function Check($key)
+	{
+		return true;
+	}
+
+}
+
+
+
+
+
+?>
